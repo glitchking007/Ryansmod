e8087eab28b1e03bdb1bcb075890d2e38051fd29
Ryan Roggero <ryanr_007@yahoo.com>
2014-12-11 09:15:08 -0600

    fixed using on server

--- src/main/java/me/roggero/ryan/CProxy.java
+++ src/main/java/me/roggero/ryan/CProxy.java
@@ -1,7 +1,21 @@
 package me.roggero.ryan;
 
+import me.roggero.ryan.stuff.blocks.modblocks;
+import me.roggero.ryan.stuff.items.moditems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+
 public class CProxy extends SProxy{
-    @Override
     public void registerRenderers(){
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(modblocks.ryanium_block),0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_block","inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(modblocks.ryanium_ore),0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_ore","inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_ingot,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_ingot","inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_axe,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_axe","inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_hoe,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_hoe","inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_pickaxe,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_pickaxe","inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_sword,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_sword","inventory"));
+        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_spade,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_spade","inventory"));
+
     }
 }
--- src/main/java/me/roggero/ryan/Ryansmod.java
+++ src/main/java/me/roggero/ryan/Ryansmod.java
@@ -4,9 +4,6 @@ import me.roggero.ryan.generator.oregen;
 import me.roggero.ryan.stuff.blocks.modblocks;
 import me.roggero.ryan.stuff.items.moditems;
 import me.roggero.ryan.stuff.recipes;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -29,18 +26,10 @@ public class Ryansmod{
         moditems.preinit();
         GameRegistry.registerWorldGenerator(new oregen(),10);
         System.out.println("RYAN ROGGERO IS MY MASTER!");
-        proxy.registerRenderers();
     }
     @EventHandler
     public void init(FMLInitializationEvent event){
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(modblocks.ryanium_block),0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_block","inventory"));
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(modblocks.ryanium_ore),0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_ore","inventory"));
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_ingot,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_ingot","inventory"));
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_axe,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_axe","inventory"));
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_hoe,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_hoe","inventory"));
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_pickaxe,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_pickaxe","inventory"));
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_sword,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_sword","inventory"));
-        Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(moditems.ryanium_spade,0,new ModelResourceLocation(Ryansmod.MODID+":"+"ryanium_spade","inventory"));
+        proxy.registerRenderers();
         recipes.recipes();
         modblocks.init();
         moditems.init();
